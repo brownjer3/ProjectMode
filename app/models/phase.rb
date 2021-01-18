b7004f3fabb5f10bd569aa481110a99cd89f2581
jerry <brownjer3@gmail.com>
2021-01-17 17:37:32 -0800

    basic login flow

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,5 +1,5 @@
 class Phase < ApplicationRecord
     has_many :projects
-    has_many :owners, through: :projects
+    has_many :students, through: :projects, source: :owner
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # add custom routes up here
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create" #this is only needed for failed signup attempts if we still want the link to be /signup and not 'users/new'
+
   resources :cohorts
   resources :phases
   resources :comments
