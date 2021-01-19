f8a24b84c9b54caa85e28affaa104c41d5ac020e
jerry <brownjer3@gmail.com>
2021-01-19 09:32:26 -0800

    project/new form setup

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,10 +5,22 @@ class ProjectsController < ApplicationController
     end
 
     def create
-        byebug
+        @project = Project.new(project_params)
+        @project.user = User.find_by(id: session[:user_id])
+        if @project.save
+            redirect_to project_path(@project)
+        else
+            #flash message here
+            render :new
+        end
     end
 
     def show
-        
+        @project = Project.find_by(id: params[:id])
+    end
+
+    private
+    def project_params
+        params.require(:project).permit(:name, :desc, :phase_id, :youtube_link, :github_link, :blog_link)
     end
 end
