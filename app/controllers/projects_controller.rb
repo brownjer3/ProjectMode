1217496710f97c08768a973cb07df6087d70a828
jerry <brownjer3@gmail.com>
2021-01-19 09:44:35 -0800

    setup for project/edit form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
 
     def create
         @project = Project.new(project_params)
-        @project.user = User.find_by(id: session[:user_id])
+        @project.owner = User.find_by(id: session[:user_id])
         if @project.save
             redirect_to project_path(@project)
         else
@@ -16,11 +16,25 @@ class ProjectsController < ApplicationController
     end
 
     def show
-        @project = Project.find_by(id: params[:id])
+        find_project
+    end
+
+    def edit
+        find_project
+    end
+
+    def update
+        find_project
+        @project.update(project_params)
+        redirect_to project_path(@project)
     end
 
     private
+    def find_project #could make this a before action for most actions in this controller
+        @project = Project.find_by(id: params[:id])
+    end
+
     def project_params
-        params.require(:project).permit(:name, :desc, :phase_id, :youtube_link, :github_link, :blog_link)
+        params.require(:project).permit(:user, :name, :desc, :phase_id, :youtube_link, :github_link, :blog_link)
     end
 end
