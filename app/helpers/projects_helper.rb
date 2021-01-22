ec1a117ad90d0da40cc5c5f53bb142d16b44511b
jerry <brownjer3@gmail.com>
2021-01-21 18:25:30 -0800

    bug fix for likers/comments modal

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -12,16 +12,18 @@ module ProjectsHelper
         project.likes.count
     end
 
-    def likers_modal
-        link_to "likes", "#likers", class: %w(text-reset text-decoration-none fw-normal), data: {bs_toggle: "modal"}
+    def likers_modal(project)
+        likes = pluralize(project.likes.count, 'like')
+        link_to likes, "#likers#{project.id}", class: %w(text-reset text-decoration-none fw-normal), data: {bs_toggle: "modal"}
     end
 
     def comment_count(project)
         project.comments.count
     end
 
-    def commenters_modal
-        link_to "comments", "#commenters", class: %w(text-reset text-decoration-none fw-normal), data: {bs_toggle: "modal"}
+    def commenters_modal(project)
+        comments = pluralize(project.comments.count, 'comment')
+        link_to comments, "#commenters#{project.id}", class: %w(text-reset text-decoration-none fw-normal), data: {bs_toggle: "modal"}
     end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   belongs_to :phase
   belongs_to :owner, class_name: "User"
   has_many :comments
-  has_many :commenters, through: :comments
+  has_many :commenters, through: :comments, source: :commenter
   has_many :likes
   has_many :likers, through: :likes, source: :user
 
