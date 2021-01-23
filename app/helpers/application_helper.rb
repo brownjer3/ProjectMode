e10f04e08b12bd410ade88e05d0b6deccfcd7ff9
jerry <brownjer3@gmail.com>
2021-01-22 23:42:17 -0800

    add name under avatar in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,18 +4,6 @@ module ApplicationHelper
       content_for :title, text
     end 
 
-    def display_avatar(user)
-      avatar = nil
-      if user.photo.attached?
-        avatar = url_for(user.photo)
-      elsif !user.image.nil?
-        avatar = user.image
-      else
-        avatar = 'technologist_avatar.png'
-      end
-      image_tag(avatar, class: %w(rounded-circle border border-2 border-secondary), height: '40', width: '40')
-    end
-
     #need to figure out where these should live
     def current_user
       @current_user ||= User.find_by_id(session[:user_id])
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,18 @@
 module UsersHelper
 
+    def display_avatar(user)
+        avatar = nil
+        if user.photo.attached?
+          avatar = url_for(user.photo)
+        elsif !user.image.nil?
+          avatar = user.image
+        else
+          avatar = 'technologist_avatar.png'
+        end
+        image_tag(avatar, class: %w(rounded-circle border border-2 border-secondary), height: '40', width: '40')
+    end
+
+
     def display_name(user)
         "#{user.first_name} #{user.last_name}"
     end
