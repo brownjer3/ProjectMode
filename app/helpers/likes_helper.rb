21e98706ebdc21375b1dd26de176b389fbef5d2d
jerry <brownjer3@gmail.com>
2021-01-23 20:36:57 -0800

    toggle like button class when liking/unliking

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,10 +1,18 @@
 module LikesHelper
 
     def like_action(project)
-        if @like = Like.find_by(project_id: project.id, user_id: current_user.id)
+        if Like.find_by(project_id: project.id, user_id: current_user.id)
             :delete
         else
             :post
+        end
+    end
+
+    def like_toggle(project)
+        if Like.find_by(project_id: project.id, user_id: current_user.id)
+            "text-secondary"
+        else
+            "text-white"
         end
     end
 
