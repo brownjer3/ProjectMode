8edb2cd86341a26448149d5de1b725002376624f
jerry <brownjer3@gmail.com>
2021-01-23 23:14:22 -0800

    add empty collection handler for homepage

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,5 +4,14 @@ module ProjectsHelper
         YouTubeAddy.extract_video_id(project.youtube_link)
     end
 
+    def empty_collection_hanlder
+        content_tag(:span, class: "row px-5") do 
+            "There are no projects here yet! #{add_project_button}".html_safe
+        end
+    end
+
+    def add_project_button
+        link_to "Add yours", new_project_path, class: "btn btn-light p-2"
+    end
 
 end
