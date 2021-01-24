7242a5cfc674f081e332ecd27b5a544f2425cb67
jerry <brownjer3@gmail.com>
2021-01-23 21:08:31 -0800

    create cohort/show page

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,2 +1,12 @@
 class CohortsController < ApplicationController
+
+    def show
+        find_cohort
+    end
+
+    private
+    def find_cohort
+        @cohort = Cohort.find_by(params[:id])
+    end
+
 end
--- app/helpers/cohorts_helper.rb
+++ app/helpers/cohorts_helper.rb
@@ -3,4 +3,14 @@ module CohortsHelper
     def projects(cohort) # need to figure out how to turn this into a scope method
         cohort.projects.take(4)
     end
+
+    def display_cohort_name(cohort)
+        "#{cohort.start_date} full-time #{cohort.focus}"
+    end
+
+    def lead_name(cohort)
+        # cohort.lead.name ||= "Jenn"
+        "with Jenn"
+    end
+
 end
